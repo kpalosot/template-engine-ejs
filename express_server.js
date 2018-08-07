91cfa4a86d09f22fd821374cd89cf4e2d7bacff3
Kyla Palos <kyla.palos@gmail.com>
2018-08-07 23:00:30 +0000

    saving project to github

---
+++ express_server.js
@@ -0,0 +1,69 @@
+var app = require("express")();
+var PORT = 8080;
+const bodyParser = require("body-parser");
+
+var urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.set("view engine", "ejs");
+
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  let newKey = generateRandomString();
+  while(Object.keys(urlDatabase).includes(newKey)){
+    newKey = generateRandomString();
+  }
+  urlDatabase[newKey] = req.body.longURL;
+  let templateVars = { urls: urlDatabase};
+  res.redirect("/urls");
+});
+
+app.post("/urls/:id/delete", (req, res) => {
+  delete urlDatabase[req.params.id];
+  res.redirect("/urls");
+});
+
+app.post("/urls/:id/update", (req, res) => {
+  urlDatabase[req.params.id] = req.body.updateURL;
+  res.redirect("/urls");
+});
+
+app.get("/urls/:id", (req, res) => {
+  let templateVars = {
+    shortURL: req.params.id,
+    longURL: urlDatabase[req.params.id]
+  };
+  res.render("urls_show", templateVars);
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  let longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
+});
+
+app.listen(PORT);
+console.log("Server listening at port", PORT);
+
+// code from at https://stackoverflow.com/questions/1349404/generate-random-string-characters-in-javascript
+function generateRandomString(){
+  let randomString = "";
+  let possibleChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  for (var i = 0; i < 7; i++) {
+    randomString += possibleChars.charAt(Math.floor(Math.random() * possibleChars.length));
+  }
+  return randomString;
+
+}
